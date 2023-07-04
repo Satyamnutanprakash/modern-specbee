e96d564158c7bac5132bf13373063cb46da18a67
Sathyam Prakaash <69395204+Satyamnutanprakash@users.noreply.github.com>
2023-07-04 21:13:35 +0530

    Add files via upload

---
+++ mspb-site/js/index.js
@@ -0,0 +1,15 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const contactButton = document.querySelector(".nav__contact__button");
+const navMenuSubMenu = document.querySelector(".nav-menu__sub-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+    contactButton.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
